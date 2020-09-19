eb589f1f0c380973a596888ac16e813b526dcc0d
Jonathan Massuchetti <jonathan.massuchetti@dappit.fr>
2020-09-19 14:58:06 +0200

    logout action

--- src/features/auth/actions/index.ts
+++ src/features/auth/actions/index.ts
@@ -3,4 +3,5 @@ import {LiskPassphrase} from "services/types";
 
 
 export const login = createAction('AUTH_LOGIN')<LiskPassphrase>();
+export const logout = createAction('AUTH_LOGOUT')();
 export const notAuthenticatedErrorAction = createAction('AUTH_NOT_AUTHENTICATED')();
